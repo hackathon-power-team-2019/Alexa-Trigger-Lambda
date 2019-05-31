b4f71c6cc9148399a36d702bf278413510d00a27
awswiz <webwizz@gmail.com>
2019-05-31 10:42:14 -0400

    fix imports.

--- TRP_Ask_Me/lambda/custom/index.js
+++ TRP_Ask_Me/lambda/custom/index.js
@@ -1,7 +1,7 @@
 "use strict";
-const main = require('./main.json');
+const Main = require('./main.json');
 const Alexa = require("alexa-sdk"); // import the library
-const Alexa = require("/flow/fund-data-flow"); // import the fund data flow
+const FundDataFlow = require("./flow/fund-data-flow"); // import the fund data flow
 
 
 //=========================================================================================================================================
