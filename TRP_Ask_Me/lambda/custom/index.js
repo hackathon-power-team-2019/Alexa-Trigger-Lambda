a13ebf0a783c4869e605a4060539d5a2685c0e5e
awswiz <webwizz@gmail.com>
2019-06-07 11:52:27 -0400

    delete commented out code.

--- TRP_Ask_Me/lambda/custom/index.js
+++ TRP_Ask_Me/lambda/custom/index.js
@@ -282,37 +282,6 @@ const WhatIsNewHandler = {
     },
 };
 
-//
-// const SubscribeToFund = {
-//     canHandle(handlerInput) {
-//         const request = handlerInput.requestEnvelope.request;
-//         return request.type === 'IntentRequest'
-//             && request.intent.name === 'SubscribeToFundIntent';
-//         //&& request.dialogState === 'COMPLETED';
-//     },
-//     handle(handlerInput) {
-//         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-//         const newFavorite = getSlotValue(handlerInput.requestEnvelope, 'fundName');
-//         const hasFavoriteFunds = sessionAttributes.hasOwnProperty("favoriteFunds") && Array.isArray(sessionAttributes.favoriteFunds);
-//         if (hasFavoriteFunds) {
-//             sessionAttributes.favoriteFunds.push(newFavorite);
-//         } else {
-//             sessionAttributes.favoriteFunds = [newFavorite];
-//         }
-//
-//         handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-//
-//         const speechText = `I saved the fund ${newFavorite} in the session attributes.
-//            Ask me for your subscribed funds to demonstrate retrieving your favorites.`;
-//         const repromptText = `You can ask me, what's my subscribed funds?`;
-//
-//         return handlerInput.responseBuilder
-//             .speak(speechText)
-//             .reprompt(repromptText)
-//             .getResponse();
-//     }
-// };
-
 const YesHandler = {
     canHandle(handlerInput) {
         const request = handlerInput.requestEnvelope.request;
